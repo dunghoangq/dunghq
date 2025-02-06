c8b9e0a5cb18841d50be3aac0b5c9d7617d5063d
dunghoangq <163510898+dunghoangq@users.noreply.github.com>
2025-02-06 08:18:13 +0000

    I don't give a fuck!

--- automation/docx_fill.py
+++ automation/docx_fill.py
@@ -1,3 +1,13 @@
 # LIBRARIES
 from docxtpl import DocxTemplate
 
+doc = DocxTemplate('my_word_template.docx')
+
+# the keys in context will replace ones in the template with their values.
+# Keys in template must be put in {{}}
+# i.e. {{company_name}}
+context = {
+    'company_name': 'Game of Thrones'
+}
+doc.render(context)
+doc.save('generated_file.docx')
